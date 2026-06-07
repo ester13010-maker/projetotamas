c4d9817cfe31d2cfd91f624c0569dabf1408c214
ester13010-maker <ester130.10@gmail.com>
2026-06-07 20:12:33 -0300

    Refactor Janela class to use ThreadSchlacht
    Esse código ta atualizado, rodando nota 10

--- Prog2/Vpls/VPL_09_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Balde) (19_outubro_2023)/Janela.py
+++ Prog2/Vpls/VPL_09_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Balde) (19_outubro_2023)/Janela.py
@@ -1,60 +1,73 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from ThreadBalde import ThreadBalde
+from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QGridLayout
+from PyQt5.QtGui import QColor
+from ThreadSchlacht import ThreadSchlacht
 
-class Janela( QWidget ):
-    __LEd1 = None
-    __PBar=None
-    __Bt1 = None
-    __MeuBalde=None
+class Janela(QWidget):
+    _LEd1 = None
+    _LEd2 = None
+    _LEd3 = None
+    _Bt_Abertura = None
+    _Bt_Fechamento = None
+    Barraca1 = None
+    Barraca2 = None
+    Barraca3 = None
 
     def __init__(self, Str="Janela", x1=400, y1=200, dx=640, dy=480, cor="orange"):
         super().__init__()
-        
         self.setWindowTitle(Str)
         self.setGeometry(x1, y1, dx, dy)
-
         self.setAutoFillBackground(True)
+        
         p = self.palette()
         p.setColor(self.backgroundRole(), QColor(cor))
         self.setPalette(p)
-
+        
         self.inicialize()
 
     def closeEvent(self, event):
-        self.__MeuBalde.parar()
+        self.Barraca1.parar()
+        self.Barraca2.parar()
+        self.Barraca3.parar()
         self.destroy()
         sys.exit(0)
-        
-    def action_executar(self):
-        if not self.__MeuBalde.isRunning():
-            self.__MeuBalde.iniciar(5)
-            self.__Bt1.setText("Parar")
-        else:
-            self.__Bt1.setText("Iniciar")
-            self.__MeuBalde.parar()
+
+    def action_abertura(self):
+        self.Barraca1.iniciar(60)
+        self.Barraca2.iniciar(60)
+        self.Barraca3.iniciar(60)
+
+    def action_fechamento(self):
+        self.Barraca1.parar()
+        self.Barraca2.parar()
+        self.Barraca3.parar()
 
     def inicialize(self):
-        Grid = QGridLayout()
-        
-        self.__PBar = QProgressBar(self)
-        self.__PBar.setOrientation(Qt.Vertical)
-        self.__PBar.setGeometry(0,0,300,25)
-        self.__PBar.setMaximum(100)
-        
-        self.__LEd1 = QLineEdit()
-        self.__Bt1 = QPushButton('Parar')
-        
-        self.__Bt1.clicked.connect(self.action_executar)
+        grid = QGridLayout()
+        self.setLayout(grid)
 
-        Grid.addWidget(self.__PBar, 0, 0, 4, 1)
-        Grid.addWidget(self.__LEd1, 1, 1, 1 ,1)
-        Grid.addWidget(self.__Bt1, 2, 1, 1, 1)
-        
-        self.__MeuBalde=ThreadBalde(self.__LEd1, self.__PBar)
+        self._LEd1 = QLineEdit()
+        self._LEd2 = QLineEdit()
+        self._LEd3 = QLineEdit()
 
-        self.setLayout(Grid)
-        self.show()
+        self._LEd1.setText('0')
+        self._LEd2.setText('0')
+        self._LEd3.setText('0')
+
+        self._Bt_Abertura = QPushButton('Abertura')
+        self._Bt_Fechamento = QPushButton('Fechamento')
+
+        self._Bt_Abertura.clicked.connect(self.action_abertura)
+        self._Bt_Fechamento.clicked.connect(self.action_fechamento)
+
+        # Adicionando os componentes ao Layout para que apareçam na tela
+        grid.addWidget(self._LEd1, 0, 0)
+        grid.addWidget(self._LEd2, 0, 1)
+        grid.addWidget(self._LEd3, 0, 2)
+        grid.addWidget(self._Bt_Abertura, 1, 0, 1, 3)
+        grid.addWidget(self._Bt_Fechamento, 2, 0, 1, 3)
+
+        self.Barraca1 = ThreadSchlacht(self._LEd1)
+        self.Barraca2 = ThreadSchlacht(self._LEd2)
+        self.Barraca3 = ThreadSchlacht(self._LEd3)
 
