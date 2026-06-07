d9c9b7562a728f53407820649e8836b6364a63bc
Estrela <ester130.10@gmail.com>
2026-06-07 20:20:55 -0300

    Enhance ThreadSchlacht with error handling and status check
    esse ta dando 10 agora, antes tava 9.13

--- Prog2/Vpls/VPL_10_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Schlachtfest) (26_outubro_2023)/ThreadSchlacht.py
+++ Prog2/Vpls/VPL_10_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Schlachtfest) (26_outubro_2023)/ThreadSchlacht.py
@@ -2,25 +2,43 @@ import random
 import threading
 import time
 
+#####################################################################
+
 class ThreadSchlacht:
     __Total=None
     __LEd=None
     __Thr=None
-
+    
     def __init__(self, LEd_a):
         self.__LEd = LEd_a
-
+        
     def iniciar(self, Total_a):
-        self.__Total = Total_a
-        self.__Thr = threading.Thread(target=self.run)
-        self.__Thr.start()
-    def parar(self):
-        self.__Total = 0;
-        self.__Thr = None;
-    def run(self):
-        while self.__Total >= 0:
-            self.__LEd.setText(str(int(self.__LEd.text()) + 1))
-            time.sleep(random.randint(1,7))
+        try:
+            if self.__Thr is None:
+                self.__Total = Total_a
+                self.__Thr = threading.Thread(target=self.run)
+                self.__Thr.start()
+        except Exception as ex:
+            print("Error: unable to start thread: %s\n" % (ex))
             
+    def parar(self):
+        try:
+            self.__Total = -1
+            self.__Thr = None
+        except Exception as ex:
+            print("Error: unable to stop thread: %s\n" % (ex))
+            
+    def isRunning(self):
+        if self.__Thr is None:
+            return False
+        else:
+            return True
+            
+    def run(self):
+        while self.__Total > 0:
+            valor = int(self.__LEd.text()) + 1
+            self.__LEd.setText(str(valor))
+            tempo = random.random()
+            time.sleep(tempo)
             self.__Total += -1
-
+        self.__Thr = None
